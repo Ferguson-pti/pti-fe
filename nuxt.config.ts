a6bc5f29b4152310bc4c36ae855cc6a353eb37ce
Chukwu Rophi <chukwurophi@gmail.com>
2025-04-25 17:28:53 +0100

    feat: intialized the project modules

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/fonts', 
+    '@vueuse/nuxt', 
+    '@nuxt/eslint',
+    'reka-ui/nuxt',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@vee-validate/nuxt'
+  ]
+})
