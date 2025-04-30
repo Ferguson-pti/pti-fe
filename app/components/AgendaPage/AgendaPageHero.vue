e7fef0cae14b8df64d62faa4d386cad3c1e49744
Chukwu Rophi <rophi.chukwu@cyphercrescent.net>
2025-04-30 22:03:07 +0100

    feat: completed the agenda page structure

--- app/components/AgendaPage/AgendaPageHero.vue
+++ app/components/AgendaPage/AgendaPageHero.vue
@@ -3,5 +3,25 @@
 </script>
 
 <template>
-  <div class="relative w-full h-auto box-border" />
+  <section class="w-full h-auto py-20 px-16 bg-custom-green flex flex-col items-start justify-start font-lexend">
+    <div class="w-full h-auto flex flex-row items-center justify-between rounded-lg overflow-hidden border border-black p-10">
+      <div class="w-[55%]">
+        <p class="text-7xl font-medium mb-6 text-white">
+          Agenda & <br> Speakers
+        </p>
+
+        <p class="mb-10 text-white text-lg font-light">
+          Dive into ICHST's most powerful moments—where extraordinary voices share groundbreaking strategies. As we prepare to bring this
+          transformative energy to San Francisco in September, get inspired by the world-class speakers who have shared the INBOUND stage and see
+          why leaders across industries choose INBOUND as their launchpad for what’s next.
+        </p>
+
+        <AppButton style-class="text-white bg-custom-red hover:bg-white hover:text-custom-red">
+          REGISTER NOW
+        </AppButton>
+      </div>
+
+      <div class="w-[500px] h-80 bg-custom-grey rounded-lg" />
+    </div>
+  </section>
 </template>
