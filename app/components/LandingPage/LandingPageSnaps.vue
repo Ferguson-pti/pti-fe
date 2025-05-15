ac542944c4eb1ae5779aba30785d1b501ef71a00
Chukwu Rophi <rophi.chukwu@cyphercrescent.net>
2025-05-15 10:06:19 +0100

    feat: completed design fix for lg screen size fixed snaps and tracks cards

--- app/components/LandingPage/LandingPageSnaps.vue
+++ app/components/LandingPage/LandingPageSnaps.vue
@@ -51,7 +51,7 @@ const renderedItems2 = urls.map((url, index) =>
       <AppSwiper
         :items="renderedItems1"
         style-class=""
-        :slides-per-view="width<=400 ? 1.2 : width<=900 ? 2.1 : 3.7"
+        :slides-per-view="width<=400 ? 1.2 : width<=900 ? 2.1 : width<=1024 ? 2.8 : 3.7"
         :pagination="false"
         :autoplay="true"
         :speed="1000"
@@ -66,7 +66,7 @@ const renderedItems2 = urls.map((url, index) =>
       <AppSwiper
         :items="renderedItems2"
         style-class=""
-        :slides-per-view="width<=400 ? 1.2 : width<=900 ? 2.1 : 3.7"
+        :slides-per-view="width<=400 ? 1.2 : width<=900 ? 2.1 : width<=1024 ? 2.8 : 3.7"
         :pagination="false"
         :autoplay="true"
         :speed="1000"
