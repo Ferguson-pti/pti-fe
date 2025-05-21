1d58748f84ea78c9757c51215d07c4ba89aa1a94
Chukwu Rophi <rophi.chukwu@cyphercrescent.net>
2025-05-21 17:30:41 +0100

    feat: finished the landing page speakers section

--- app/components/LandingPage/LandingPageSpeakers.vue
+++ app/components/LandingPage/LandingPageSpeakers.vue
@@ -3,29 +3,31 @@
 </script>
 
 <template>
-  <section class="w-full py-10 lg:py-20 px-4 md:px-10 xl:px-16 bg-custom-red flex flex-col items-center justify-center font-lexend">
-    <div class="w-full py-4 md:py-10 rounded-lg">
-      <h1 class="text-4xl md:text-5xl lg:text-7xl text-white w-[55%] mb-6 font-medium">
-        Featured Speakers
-      </h1>
+  <AppSectionWrapper bg="bg-custom-red">
+    <section class="w-full lg:py-10 bg-custom-red flex flex-col items-center justify-center font-lexend">
+      <div class="w-full py-4 md:py-20 rounded-lg">
+        <p class="text-2xl md:text-4xl xl:text-5xl 2xl:text-6xl text-white mb-6 font-medium">
+          Featured Speakers
+        </p>
 
-      <p class="text-white w-full lg:w-[%] mt-4 font-light text-base md:text-lg mb-0 md:mb-10">
-        ICHST lands in Abuja with its strongest program yet, themed Transforming Africa’s Hydrocarbon Sector: Balancing Growth, Environment,
-        and Governance. From sustainable financing to cutting-edge tech and ethical leadership, industry visionaries will share insights to
-        drive impactful change across Africa’s energy landscape.
-      </p>
+        <p class="text-white w-full lg:w-[80%] xl:w-[70%] mt-4 font-light text-sm md:text-base 2xl:text-xl mb-0 md:mb-10">
+          ICHST lands in Abuja with its strongest program yet, themed Transforming Africa’s Hydrocarbon Sector: Balancing Growth, Environment,
+          and Governance. From sustainable financing to cutting-edge tech and ethical leadership, industry visionaries will share insights to
+          drive impactful change across Africa’s energy landscape.
+        </p>
 
-      <AppSpeakersSwiper
-        style-class="w-full md:w-full h-[380px] md:h-[400px]"
-        :pagination="{ clickable: true }"
-        :autoplay="false"
-        color="white"
-        :slides-per-view="`auto`"
-        :space-between="20"
-        :loop="false"
-      />
-    </div>
-  </section>
+        <AppSpeakersSwiper
+          style-class="w-full md:w-full h-[380px] md:h-[400px]"
+          :pagination="{ clickable: true }"
+          :autoplay="false"
+          color="white"
+          :slides-per-view="`auto`"
+          :space-between="20"
+          :loop="false"
+        />
+      </div>
+    </section>
+  </AppSectionWrapper>
 </template>
 
 <style>
