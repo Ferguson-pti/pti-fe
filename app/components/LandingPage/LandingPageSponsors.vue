9a184114b02011114eab1d72b5468f8b3027b687
Chukwu Rophi <chukwurophi@gmail.com>
2025-06-19 21:54:08 +0100

    fix: updated the sponsors arrangement

--- app/components/LandingPage/LandingPageSponsors.vue
+++ app/components/LandingPage/LandingPageSponsors.vue
@@ -5,22 +5,25 @@
 <template>
   <AppSectionWrapper bg="bg-white">
     <section class="w-full py-14 md:py-20 flex flex-col items-center justify-start font-lexend">
-      <span class="text-2xl md:text-4xl xl:text-5xl 2xl:text-6xl font-medium text-custom-green">Partners</span>
+      <span class="text-2xl md:text-4xl xl:text-5xl 2xl:text-6xl font-medium text-custom-green">Host</span>
+      <div class="mt-6 flex flex-col items-center justify-center">
+        <NuxtImg
+          src="/pti.webp"
+          provider="sirv"
+          alt="logo"
+          class="h-20 md:h-32 xl:h-32"
+        />
+      </div>
 
-      <div class="mt-12 w-full flex flex-col items-center justify-start gap-4 md:gap-10 flex-wrap lg:px-20">
+      <span class="mt-12 text-2xl md:text-4xl xl:text-5xl 2xl:text-6xl font-medium text-custom-green">Partners</span>
+
+      <div class="mt-6 w-full flex flex-col items-center justify-start gap-4 md:gap-10 flex-wrap lg:px-20 xl:px-40">
         <div class="flex flex-row items-center justify-center gap-4 md:gap-10 flex-wrap">
           <NuxtImg
             src="/nnpc.webp"
             provider="sirv"
             alt="logo"
             class="h-16 md:h-24 xl:h-24"
-          />
-
-          <NuxtImg
-            src="/pti.webp"
-            provider="sirv"
-            alt="logo"
-            class="h-20 md:h-32 xl:h-32"
           />
 
           <NuxtImg
@@ -45,12 +48,6 @@
           />
 
           <NuxtImg
-            src="/ccl.webp"
-            provider="sirv"
-            alt="logo"
-            class="h-10 md:h-16 xl:h-16"
-          />
-          <NuxtImg
             src="/nuprc.webp"
             provider="sirv"
             alt="logo"
@@ -58,7 +55,14 @@
           />
 
           <NuxtImg
-            src="/nmdpralogo.webp"
+            src="/ccl.webp"
+            provider="sirv"
+            alt="logo"
+            class="h-10 md:h-16 xl:h-16"
+          />
+
+          <NuxtImg
+            src="/nmdrpa1.webp"
             provider="sirv"
             alt="logo"
             class="h-20 md:h-32 xl:h-32"
